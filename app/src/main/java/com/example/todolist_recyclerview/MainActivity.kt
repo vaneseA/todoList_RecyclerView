21c08f6198f21c0458486eeba0019ee5a22334ed
DK <spol@naver.com>
2022-05-27 21:56:03 +0900

    4. bottomNav와 프래그먼트 연결

--- app/src/main/java/com/example/todolist_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/todolist_recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todolist_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.example.todolist_recyclerview.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,5 +23,23 @@ class MainActivity : AppCompatActivity() {
     private fun  setValues() {
         mPagerAdapter = MainViewPagerAdapter(this)
         mainViewPage.adapter = mPagerAdapter
+
+//      바텀네비게이션 클릭이벤트처리
+        bottomNav.setOnItemSelectedListener {
+           when(it.itemId) {
+               R.id.home -> mainViewPage.currentItem = 0
+               R.id.paper -> mainViewPage.currentItem= 1
+               R.id.profile -> mainViewPage.currentItem= 2
+           }
+            return@setOnItemSelectedListener true
+        }
+
+//        뷰 페이저 연결
+        mainViewPage.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            bottomNav.menu.getItem(position).isChecked = true
+            }
+        })
     }
 }
