ea8caa4e41d685a677937790bc56cef3afa9b912
DK <spol@naver.com>
2022-05-27 22:08:44 +0900

    -detail Activity 생성 및 xml 작성

---
+++ app/src/main/java/com/example/todolist_recyclerview/DetailTodoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.todolist_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_todo)
+    }
+}
